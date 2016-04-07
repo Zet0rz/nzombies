3d2c3045efebf1240050d768bcce57a11fb15eb1
Michael Smith <ScriptnLua@users.noreply.github.com>
2016-04-07 12:46:45 -0400

    Updated shared.lua with comments
    so we don't look back later and go "What does this even do?"

--- lua/entities/breakable_entry/shared.lua
+++ lua/entities/breakable_entry/shared.lua
@@ -1,14 +1,19 @@
 AddCSLuaFile( )
-
+-- Defining what type of entity this is
 ENT.Type = "anim"
 
+-- Defining what to call it by, the author, how to contact him/her, the purpose, and instructions on how to use it
 ENT.PrintName		= "breakable_entry"
 ENT.Author			= "Alig96"
 ENT.Contact			= "Don't"
 ENT.Purpose			= ""
 ENT.Instructions	= ""
+
+-- Loading models to use
 //models/props_interiors/elevatorshaft_door01a.mdl
 //models/props_debris/wood_board02a.mdl
+
+-- INIT Section
 function ENT:Initialize()
 
 	self:SetModel("models/props_c17/fence01b.mdl")
@@ -25,13 +30,17 @@ function ENT:Initialize()
 		self:ResetPlanks(true)
 	end
 end
+-- End of INIT section
 
+-- Creating data tables
 function ENT:SetupDataTables()
 
+	-- Defining a network variable to decide how many placks to use later on
 	self:NetworkVar( "Int", 0, "NumPlanks" )
 
 end
 
+-- Function that adds the planks
 function ENT:AddPlank(nosound)
 	self:SpawnPlank()
 	self:SetNumPlanks( (self:GetNumPlanks() or 0) + 1 )
@@ -40,6 +49,7 @@ function ENT:AddPlank(nosound)
 	end
 end
 
+-- Function to remove the planks
 function ENT:RemovePlank()
 
 	local plank = table.Random(self.Planks)
@@ -62,6 +72,7 @@ function ENT:RemovePlank()
 	end
 end
 
+-- Function to reset the planks
 function ENT:ResetPlanks(nosoundoverride)
 	for i=1, GetConVar("nz_difficulty_barricade_planks_max"):GetInt() do
 		self:RemovePlank()
@@ -71,6 +82,7 @@ function ENT:ResetPlanks(nosoundoverride)
 	end
 end
 
+-- Function that gives the player points after fixing the planks/barricade
 function ENT:Use( activator, caller )
 	if CurTime() > self.NextPlank then
 		if self:GetNumPlanks() < GetConVar("nz_difficulty_barricade_planks_max"):GetInt() then
@@ -82,6 +94,7 @@ function ENT:Use( activator, caller )
 	end
 end
 
+-- Function to spawn planks
 function ENT:SpawnPlank()
 	//Spawn
 	local angs = {-60,-70,60,70}
@@ -94,6 +107,7 @@ function ENT:SpawnPlank()
 	table.insert(self.Planks, plank)
 end
 
+-- Defining that zombies should collide with the barricade rather than go through them
 hook.Add("ShouldCollide", "zCollisionHook", function(ent1, ent2)
 	if ent1:GetClass() == "breakable_entry" and (nz.Config.ValidEnemies[ent2:GetClass()]) then
 		if ent1:IsValid() and ent1:GetNumPlanks() == 0 then
@@ -111,6 +125,7 @@ hook.Add("ShouldCollide", "zCollisionHook", function(ent1, ent2)
 	end
 end)
 
+-- Client side operations to draw board models
 if CLIENT then
 	function ENT:Draw()
 		if Round:InState( ROUND_CREATE ) then
