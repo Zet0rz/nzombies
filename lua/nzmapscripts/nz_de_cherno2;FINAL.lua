880ddac114f9c23c39fb6b9f863c8bdc03b3871d
Logan <lobsterlogan43@yahoo.com>
2016-07-20 16:25:49 -0500

    idk

--- lua/nzmapscripts/nz_de_cherno2;FINAL.lua
+++ lua/nzmapscripts/nz_de_cherno2;FINAL.lua
@@ -3,6 +3,9 @@ local mapscript = {}
 function mapscript.OnGameBegin()
 	mapscript.genButton = ents.FindByName("GenButton01")[1]
     mapscript.IsElectricityOn = false
+    for k, v in pairs( player.GetAll() ) do
+        v.CanUseFlashLight = false
+    end
     engine.LightStyle( 0, "d" )
     for k, v in pairs( player.GetAll() ) do
         v:SendLua( "render.RedownloadAllLightmaps()" )
@@ -10,8 +13,8 @@ function mapscript.OnGameBegin()
 end
 
 function mapscript.RoundStart()
-    for k, v in pairs player.GetAll() do
-        v:AllowFlashlight( IsElec() )
+    for k, v in pairs( player.GetAll() ) do
+        v:AllowFlashlight(  )
     end
 end
 
